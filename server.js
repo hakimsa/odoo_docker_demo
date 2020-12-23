0ddaa779da64dd18929409a7a1d41e777fd76fc7
Kim <samouh591@gmail.com>
2020-12-24 00:15:39 +0100

    docker mdof

--- server.js
+++ server.js
@@ -9,3 +9,6 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
