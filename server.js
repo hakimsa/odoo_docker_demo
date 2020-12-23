e4b3378e8509cf97ba3060e94783e4cda750560b
Kim <samouh591@gmail.com>
2020-12-24 00:23:01 +0100

    docker mdof

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express');
-
-// Constants
-
-
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('<h1>Hello World</h1>');
 });
 
 app.listen(process.env.PORT || 3000, function(){
