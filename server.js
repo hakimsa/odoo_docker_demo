2bcc5a51e8639f8e9e6175b16023357294413764
Kim <samouh591@gmail.com>
2020-12-24 10:22:35 +0100

    docker mdof

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
+path = require("path"),
+    app = express(),
+    puerto = 3000;
+
+app.get('/', (peticion, respuesta) => {
+    // Podemos acceder a la petición HTTP
+    let agenteDeUsuario = peticion.header("user-agent");
+    respuesta.send("La ruta / solicitada con: " + agenteDeUsuario);
 });
+app.get('/pagina', (peticion, respuesta) => {
+    // Servir archivo HTML, o cualquier otro archivo
+    let rutaDeArchivo = path.join(__dirname, "index.html");
+    respuesta.sendFile(rutaDeArchivo);
+});
+
+app.get('/hola', (peticion, respuesta) => {
+    let mascota = {
+        nombre: "Maggie",
+        edad: 2,
+    };
+    respuesta.json(mascota);
+});
+
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
